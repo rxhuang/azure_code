de6296477fba86a735e029cda08314432c03bf01
Ruoxin Huang <rxhuang@live.ca>
2021-05-13 22:48:28 -0700

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,35 @@
+import logging
+import json
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    n = req.params.get('n')
+    if not n:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            n = req_body.get('n')
+
+    if n:
+        data = {'fibonacciNumber': fib(int(n)), 'n': n}
+        out = json.dumps(data)
+        return func.HttpResponse(out)
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+def fib (n):  
+    arr = [0,1]
+
+    for i in range (n-1):
+        arr += [arr[-1] + arr[-2]]
+
+    return arr[-1]
